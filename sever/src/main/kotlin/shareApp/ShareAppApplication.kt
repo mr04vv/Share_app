6190725552cc7158a3d332bc0a82b7236a2205a9
mori takuto <mr04vv@gmail.com>
2018-03-01 17:28:03 +0900

    herokuデプロイ

--- sever/src/main/kotlin/shareApp/ShareAppApplication.kt
+++ sever/src/main/kotlin/shareApp/ShareAppApplication.kt
@@ -1,15 +1,15 @@
 package shareApp
 
 import shareApp.conf.Routes
-import shareApp.db.dbConnect
+import shareApp.db.dbConnectHeroku
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) { 
 
-
-    dbConnect() //データベース接続
+    // dbConnect() //データベース接続
+    dbConnectHeroku()
     filter() //cors許容
     Routes().init()
-
+    
 //    path("/users") {
 //        get("", UserController().getUser(), toJson)
 //        get("/me", UserController().getUserMe(), toJson)
--- sever/src/main/kotlin/shareApp/db/connection.kt
+++ sever/src/main/kotlin/shareApp/db/connection.kt
@@ -17,3 +17,16 @@ fun dbConnect() {
         create(Token_t)
     }
 }
+
+fun dbConnectHeroku() {
+    Database.connect("jdbc:postgresql://ec2-184-73-175-95.compute-1.amazonaws.com:5432/d6is229dd8d5he?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "com.mysql.jdbc.Driver","gdotzlmesfdrwv","aca5abbf7b12ecbedb441df86f58c254e02373acc1549737918d45a46f230b04")
+    transaction {
+        create(User_t)
+        create(Task_t)
+        create(Content_t)
+        create(Comment_t)
+        create(Group_t)
+        create(GroupMember_t)
+        create(Token_t)
+    }
+}
