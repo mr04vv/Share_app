ddcf675ce1775afd32ab7e742ddc866c33a2346b
mori takuto <mr04vv@gmail.com>
2018-06-01 23:59:43 +0900

    refactor

--- sever/src/main/kotlin/shareApp/model/content.kt
+++
@@ -1,9 +0,0 @@
-package shareApp.model
-
-import org.jetbrains.exposed.sql.*
-
-object Content_t : Table("contents") {
-    val id = integer("id").autoIncrement().primaryKey()
-    val title = varchar("title", 50)
-    val url = varchar("url", 100)
-}
